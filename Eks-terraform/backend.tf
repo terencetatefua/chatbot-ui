0165ff60303e10333f4c30f20f95d9d320cd75de
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2024-06-02 14:57:28 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "chat-bot-project" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "chatbot/terraform.tfstate"
     region = "us-east-2"
   }
 }
